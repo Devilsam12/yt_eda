fb1f80effdb897d86108150e66542025c8f08dfc
Samyak Meshram <sam.meshram18@gmail.com>
2023-08-27 23:08:23 -0400

    some changes

--- yt_eda/__init__.py
+++ yt_eda/__init__.py
@@ -1,4 +1,4 @@
-from .preprocessor import Preprocessor
-from .visualization import BeforePreprocessingVisualizer, AfterPreprocessingVisualizer
+from .preprocessor import DataPreprocessor
+from .visualization import PreprocessVisualizer, PostprocessVisualizer
 from .feature_engineering import FeatureEngineer
 from .model import Model
